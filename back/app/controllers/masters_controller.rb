836796d5641c861e6da468c29b0ef1db52081be5
Keita <keitahighbridge@gmail.com>
2020-04-16 21:20:47 +0900

    税務細目の連携まで完成

--- back/app/controllers/masters_controller.rb
+++ back/app/controllers/masters_controller.rb
@@ -1,23 +1,31 @@
 class MastersController < ApplicationController
 
   def useful_life
-    all = UsefulLife.all
-    render json: {data: all}
+    all_useful_lives = UsefulLife.all
+    render json: {data: all_useful_lives}
   end
 
   def asset_type
-    all = AssetType.all
-    render json: {data: all}
+    all_types = AssetType.all
+    render json: {data: all_types}
   end
 
   def asset_group
-    all = AssetGroup.all
-    render json: {data: all}
+    all_groups = AssetGroup.all
+    render json: {data: all_groups}
   end
 
   def asset_item
-    all = AssetItem.all
-    render json: {data: all}
+    all_items = AssetItem.all
+    all_items_response = []
+    all_items.each do |item|
+      all_items_response.push(
+        group: item.asset_group.name,
+        item: item.item.split(','),
+        useful_life: item.useful_life.year
+      )
+    end
+    render json: {items: all_items_response}
   end
 
 end
--- front/src/components/NewAsset.tsx
+++ front/src/components/NewAsset.tsx
@@ -1,22 +1,39 @@
-import React, {useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import styled from 'styled-components'
 
 import {Container} from '../materials/Container'
 import {H2} from '../materials/Text'
 import {StyledButton} from '../materials/Button'
-import {getApiData} from '../utilities/getApiData'
 import {GET_ASSET_ITEM_URL} from '../utilities/urls'
 
 const NewAsset = () => {
 
+  const [assetItems, setAssetItems] = useState([])
+
   useEffect(() => {
     const getData = async () => {
-      const data = await getApiData(GET_ASSET_ITEM_URL)
-      console.log(data)
-    } 
+      const { data: { items } } = await axios.get(GET_ASSET_ITEM_URL)
+      setAssetItems(items)
+    }
     getData()
   }, [])
-  
+
+  let AssetItems = assetItems.map((value: any, index: number) => {
+    return (
+      <tr key={index}>
+        <td>{value.group}</td>
+        <td>{value.item[0]}</td>
+        <td>{value.item[1]}</td>
+        <td>{value.item[2]}</td>
+        <td>{value.item[3]}</td>
+        <td>{value.item[4]}</td>
+        <td>{value.item[5]}</td>
+        <td>{value.item[6]}</td>
+        <td>{value.useful_life}</td>
+      </tr>
+    )
+  })
 
   return (
     <Container>
@@ -27,6 +44,11 @@ const NewAsset = () => {
       <StyledDiv>
         <StyledButton>登録する</StyledButton>
       </StyledDiv>
+      <table>
+        <tbody>
+          {AssetItems}
+        </tbody>
+      </table>
     </Container>
   );
 };
